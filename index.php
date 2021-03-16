7c6db74a440d3ca2c1f947ee73f415334a1770c6
airinanilgithub <mca501@rajagiri.edu>
2021-03-17 00:18:32 +0530

    message

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Department</title>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-success">        <div class="container-fluid">
+          <a class="navbar-brand" href="#"><b>Department of Computer Application</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="gallery.php">Gallery</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="departments.php">Departments</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="aboutus.php">About Us</a>
+              </li>
+              
+              
+            </ul>
+            
+          </div>
+        </div>
+      </nav>
+<marquee>PG -2021 Admissions open for MCA,MSC Computer Sciences(Data Analytics),Application for the post of Assistant Professor</marquee>
+      <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img height="600px" width="5200px" src="https://dc-cdn.s3-ap-southeast-1.amazonaws.com/dc-Cover-8aab18mkr3gr4a7nef9nhcoc00-20160426060743.Medi.jpeg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              
+              <h1><p>Welcome to Department of Social Sciences</p></h1>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img height="600px" width="5200px" src="http://dyuti.in/assets/images/rcss/full_college.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h1><p>Welcome to Department of Social Sciences</p></h1>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img height="600px" width="4000" src="https://cache.careers360.mobi/media/presets/720X480/colleges/social-media/media-gallery/9220/2018/10/9/Lib.%20of%20Rajagiri%20College%20of%20Social%20Sciences%20Ernakulam_Library.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h1><p>Welcome to Department of Social Sciences</p></h1>
+            </div>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"  data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"  data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
